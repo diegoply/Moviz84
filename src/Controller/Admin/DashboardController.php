fc3c3332a23a1be505b071b61776a24a4ad43401
Diego Ply <ply.dgo@gmail.com>
2024-03-21 19:17:45 +0100

    debut daxhboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Director;
+use App\Entity\Genre;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -41,6 +43,7 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Genre', 'fas fa-list', Genre::class);
+        yield MenuItem::linkToCrud('Réalisateurs', 'fa-solid fa-clapperboard', Director::class);
     }
 }
