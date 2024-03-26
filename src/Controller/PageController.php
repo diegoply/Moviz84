f84d7ead86082f20a7a9963e8fc5dcf1ffc4c519
Diego Ply <ply.dgo@gmail.com>
2024-03-26 17:51:17 +0100

    fin projet

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -20,6 +21,15 @@ class PageController extends AbstractController
           'movies' => $movies,
         ]);
     }
+    public function genreIndex(MovieRepository $movieRepository, Request $request): Response
+    {
+        $genreId = $request->get('genreId');
+        $movies = $movieRepository->findMovies($genreId);
+
+        return $this->render('movie/index.html.twig', [
+            'movies' => $movies,
+        ]);
+    }
 
     #[Route('/about', name: 'app_about')]
     public function about(): Response
@@ -29,5 +39,7 @@ class PageController extends AbstractController
             'websiteName' => $websiteName,
         ]);
     }
+
+    
 
 }
